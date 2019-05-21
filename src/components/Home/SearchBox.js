160067ef006a3623e544c39a36887548bea34613
Marcelo <cecel.alex@gmail.com>
2019-05-21 13:16:12 -0300

    Adicionando campo de busca, buscando destino e tracando a rota

---
+++ src/components/Home/SearchBox.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
+import { makeLocationSeach } from '../../DevsUberAPI';
+import SearchBoxItem from './SearchBoxItem';
+
+export default class SearchBox extends Component {
+    timer = null;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            txt:'',
+            results:[]
+        };
+
+        this.txtFill = this.txtFill.bind(this);
+        this.doSeach = this.doSeach.bind(this);
+    }
+
+    txtFill(text) {
+        this.setState({txt:text, results:[]});
+
+        if(typeof this.timer != null) {
+            clearTimeout(this.timer);
+        }
+
+        this.timer = setTimeout(this.doSeach, 1500);
+    }
+
+    doSeach() {
+        makeLocationSeach(this.state.txt)
+            .then((results)=>{
+                this.setState({results});
+            })
+            .catch((error)=>{
+                alert("Ocorreu um error...");
+            });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.box}>
+                    <TextInput style={styles.input} value={this.state.txt} onChangeText={this.txtFill} placeholder="Digite seu destino..." />
+                </View>
+                {this.state.results.length > 0 && 
+                    <ScrollView style={styles.result}>
+                        {this.state.results.map((item)=>{
+                            return(
+                                <SearchBoxItem dataClick={()=>{
+                                    this.props.dataClick(item);
+                                    this.setState({results:[], txt:item.label});
+                                }} key={item.id} data={item} />
+                            );
+                        })}
+                    </ScrollView>
+                }
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        position:'absolute',
+        width:'100%',
+        height:'100%',
+        alignItems:'center'
+    },
+    box:{
+        width:'80%',
+        height:50,
+        marginTop:40,
+        backgroundColor:'#FFFFFF',
+        top:40,
+        borderRadius:3,
+        borderWidth:1,
+        borderColor:'#CCCCCC',
+        elevation:4,
+        shadowOffset:{width:20, height:20},
+        shadowColor:'#000000',
+        shadowOpacity:0.5,
+        shadowRadius:10
+    },
+    input:{
+        width:'100%',
+        height:'100%',
+        padding:10,
+        fontSize:19
+    },
+    result:{
+        width:'80%',
+        marginBottom:40,
+        backgroundColor:'#FFFFFF',
+        borderRadius:3,
+        borderWidth:1,
+        borderColor:'#CCCCCC'
+    }
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,11 @@ import { View, Text, StyleSheet, Animated, Platform, PermissionsAndroid } from '
 import { connect } from 'react-redux';
 import MapView from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+import MapViewDirections from 'react-native-maps-directions';
+
+import SearchBox from '../components/Home/SearchBox';
+
+const GOOGLE_MAPS_APIKEY = "AIzaSyA2KeS4ZYdDSEMalH86f_PAIukTNGJsNDo";
 
 export class Home extends Component {
     watchId = null;
@@ -29,6 +34,10 @@ export class Home extends Component {
                 latitudeDelta:0.004,
                 longitudeDelta:0.004
             },
+            destLocation:{
+                latitude:0,
+                longitude:0
+            },
             isLoading:false,
             loadingMsg:'',
             warnHeight:new Animated.Value(0)
@@ -37,6 +46,7 @@ export class Home extends Component {
         this.setWarning = this.setWarning.bind(this);
         this.getCurrentLoction = this.getCurrentLoction.bind(this);
         this.requestLocPermission = this.requestLocPermission.bind(this);
+        this.seachBoxClick = this.seachBoxClick.bind(this);
     }
 
     componentDidMount() {
@@ -122,6 +132,15 @@ export class Home extends Component {
         }
     }
 
+    seachBoxClick(item) {
+        this.setState({
+            destLocation:{
+                latitude:item.lat,
+                longitude:item.lng
+            }
+        });
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -129,10 +148,24 @@ export class Home extends Component {
                     style={styles.map}
                     region={this.state.currentLocation}
                 >
+                    <MapView.Marker coordinate={this.state.currentLocation} />
+                    {this.state.destLocation.latitude != 0 &&
+                        <MapView.Marker coordinate={this.state.destLocation} />
+                    }
+                    {this.state.destLocation.latitude != 0 &&
+                        <MapViewDirections
+                            origin={this.state.currentLocation}
+                            destination={this.state.destLocation}
+                            apikey={GOOGLE_MAPS_APIKEY}
+                            strokeWidth={5}
+                            strokeColor="#000000"
+                        />
+                    }
                 </MapView>
                 <Animated.View style={[styles.warnBox, {height:this.state.warnHeight}]}>
                     <Text style={styles.warnText}>{this.state.loadingMsg}</Text>
                 </Animated.View>
+                <SearchBox dataClick={this.seachBoxClick} />
             </View>
         );
     }
